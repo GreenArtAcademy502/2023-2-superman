14c541c95f3728120ecf7472ba6ed59d3832dc9a
DoheumPark <pirbak@hanmail.net>
2023-12-06 09:47:42 +0900

    first

---
+++ src/main/java/com/green/superman/SupermanApplication.java
@@ -0,0 +1,13 @@
+package com.green.superman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupermanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/superman/SupermanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.superman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
